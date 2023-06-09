fe58e1ce0bd1bf3443c4c1157c5a4d3e4c3ef8d1
Vladislav <107634092+zcredi@users.noreply.github.com>
2023-06-09 17:14:19 +0300

    add project

---
+++ MyMusicApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MyMusicApp
+//
+//  Created by Владислав on 09.06.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+var t = 1
+}
+
