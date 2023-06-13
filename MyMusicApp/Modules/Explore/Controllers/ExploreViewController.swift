aea959c73c2376ceb1511135a4f160c1d0a4f3d1
Vladislav <107634092+zcredi@users.noreply.github.com>
2023-06-13 16:32:05 +0300

    add Explore

---
+++ MyMusicApp/Modules/Explore/Controllers/ExploreViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ExploreViewController.swift
+//  MyMusicApp
+//
+//  Created by Владислав on 13.06.2023.
+//
+
+import UIKit
+
+class ExploreViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    
+    }
+
+
+}
