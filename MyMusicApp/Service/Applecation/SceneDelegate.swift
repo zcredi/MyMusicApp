85ec26cadde5534e3f2e9cdd2aa4bc3eb189eb7b
Vladislav <107634092+zcredi@users.noreply.github.com>
2023-06-24 12:38:21 +0300

    add navigationController

--- MyMusicApp/Service/Applecation/SceneDelegate.swift
+++ MyMusicApp/Service/Applecation/SceneDelegate.swift
@@ -8,53 +8,65 @@
 import UIKit
 
 class SceneDelegate: UIResponder, UIWindowSceneDelegate {
-
+    
     var window: UIWindow?
-
-
+    
+    
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
         guard let windowScene = (scene as? UIWindowScene) else { return }
         window = UIWindow(windowScene: windowScene)
+        
         let viewController = MainTabBarController()
+        let navigationController = UINavigationController(rootViewController: viewController)
+        
         if #available(iOS 15.0, *) {
             let appearance = UINavigationBarAppearance()
-                appearance.configureWithOpaqueBackground()
-                appearance.backgroundColor = .systemBackground
-                UINavigationBar.appearance().standardAppearance = appearance
-                UINavigationBar.appearance().scrollEdgeAppearance = appearance
-            window?.rootViewController = viewController
+            appearance.configureWithOpaqueBackground()
+            appearance.backgroundColor = .brandBlack
+            
+            // устанавливаем цвет и изображение для кнопки назад
+            navigationController.navigationBar.standardAppearance = appearance
+            navigationController.navigationBar.scrollEdgeAppearance = appearance
+            let backButtonImage = UIImage(named: "arrow_back")?.withRenderingMode(.alwaysOriginal)
+            navigationController.navigationBar.backIndicatorImage = backButtonImage
+            navigationController.navigationBar.backIndicatorTransitionMaskImage = backButtonImage
+            navigationController.navigationBar.tintColor = .neutralWhite
+            
+            // скрываем текст на кнопке назад
+            viewController.navigationItem.backButtonTitle = ""
+            window?.rootViewController = navigationController
             window?.makeKeyAndVisible()
             window?.overrideUserInterfaceStyle = .unspecified
         }
     }
-
+    
     func sceneDidDisconnect(_ scene: UIScene) {
         // Called as the scene is being released by the system.
         // This occurs shortly after the scene enters the background, or when its session is discarded.
         // Release any resources associated with this scene that can be re-created the next time the scene connects.
         // The scene may re-connect later, as its session was not necessarily discarded (see `application:didDiscardSceneSessions` instead).
     }
-
+    
     func sceneDidBecomeActive(_ scene: UIScene) {
         // Called when the scene has moved from an inactive state to an active state.
         // Use this method to restart any tasks that were paused (or not yet started) when the scene was inactive.
     }
-
+    
     func sceneWillResignActive(_ scene: UIScene) {
         // Called when the scene will move from an active state to an inactive state.
         // This may occur due to temporary interruptions (ex. an incoming phone call).
     }
-
+    
     func sceneWillEnterForeground(_ scene: UIScene) {
         // Called as the scene transitions from the background to the foreground.
         // Use this method to undo the changes made on entering the background.
     }
-
+    
     func sceneDidEnterBackground(_ scene: UIScene) {
         // Called as the scene transitions from the foreground to the background.
         // Use this method to save data, release shared resources, and store enough scene-specific state information
         // to restore the scene back to its current state.
     }
-
-
+    
+    
 }
